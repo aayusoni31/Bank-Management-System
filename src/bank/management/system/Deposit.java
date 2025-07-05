0d52a6bfe7264c1e59028a56928607867dec1fc9
Aayushi Verma <137631538+aayusoni31@users.noreply.github.com>
2025-07-05 23:48:41 +0530

    added database

--- src/bank/management/system/Deposit.java
+++ src/bank/management/system/Deposit.java
@@ -18,10 +18,10 @@ public class Deposit extends JFrame implements ActionListener {
         Image i2 = i1.getImage().getScaledInstance(1550,830,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel l3 = new JLabel(i3);
-        l3.setBounds(0,0,1550,830);
+        l3.setBounds( 0,0,1550,830);
         add(l3);
 
-        JLabel label1 = new JLabel("ENETR AMOUNT YOU WANT TO DEPOSIT");
+        JLabel label1 = new JLabel("ENTER AMOUNT YOU WANT TO DEPOSIT");
         label1.setForeground(Color.WHITE);
         label1.setFont(new Font("System", Font.BOLD, 16));
         label1.setBounds(460,180,400,35);
